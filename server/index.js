e7d4c546676e39f179edd64d0250638e94b1e5de
dreamarena38-oss <dreamarena38@gmail.com>
2026-07-22 12:04:32 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 require('dotenv').config();
-
 const authRoutes = require('./routes/auth.routes');
 const groundRoutes = require('./routes/ground.routes');
 const bookingRoutes = require('./routes/booking.routes');
@@ -16,7 +15,11 @@ const seedMockData = require('./configs/mockSeeder');
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
